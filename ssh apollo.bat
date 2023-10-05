34310f1deec61fd7302595ecfc2e49dc7b15a337
n01635529 <n01635529@humber.ca>
2023-10-05 19:15:47 -0400

    ssh for operating systems

---
+++ ssh apollo.bat
@@ -0,0 +1,51 @@
+@echo off
+color d
+title connecting to apollo
+echo @apollo.humber.ca
+echo.
+echo                   connecting to apollo
+echo                 connecting to apollo
+echo               connecting to apollo
+echo             connecting to apollo
+echo           connecting to apollo
+echo         connecting to apollo
+echo       connecting to apollo
+echo     connecting to apollo
+echo   connecting to apollo
+echo.
+echo made by kokotf
+:: timeout not required just for aesthetic reasons
+timeout /t 3 /nobreak >nul
+cls
+echo.
+echo           _____                     _____                     _____          
+echo          /\    \                   /\    \                   /\    \         
+echo         /::\    \                 /::\    \                 /::\____\        
+echo        /::::\    \               /::::\    \               /:::/    /        
+echo       /::::::\    \             /::::::\    \             /:::/    /         
+echo      /:::/\:::\    \           /:::/\:::\    \           /:::/    /          
+echo     /:::/__\:::\    \         /:::/__\:::\    \         /:::/____/           
+echo     \:::\   \:::\    \        \:::\   \:::\    \       /::::\    \           
+echo   ___\:::\   \:::\    \     ___\:::\   \:::\    \     /::::::\    \   _____  
+echo  /\   \:::\   \:::\    \   /\   \:::\   \:::\    \   /:::/\:::\    \ /\    \ 
+echo /::\   \:::\   \:::\____\ /::\   \:::\   \:::\____\ /:::/  \:::\    /::\____\
+echo \:::\   \:::\   \::/    / \:::\   \:::\   \::/    / \::/    \:::\  /:::/    /
+echo  \:::\   \:::\   \/____/   \:::\   \:::\   \/____/   \/____/ \:::\/:::/    / 
+echo   \:::\   \:::\    \        \:::\   \:::\    \                \::::::/    /  
+echo    \:::\   \:::\____\        \:::\   \:::\____\                \::::/    /   
+echo     \:::\  /:::/    /         \:::\  /:::/    /                /:::/    /    
+echo      \:::\/:::/    /           \:::\/:::/    /                /:::/    /     
+echo       \::::::/    /             \::::::/    /                /:::/    /      
+echo        \::::/    /               \::::/    /                /:::/    /       
+echo         \::/    /                 \::/    /                 \::/    /        
+echo          \/____/                   \/____/                   \/____/  
+echo.      
+title connected to apollo
+::
+::
+::
+::
+:: Change student number to your own to connect to your profile
+::  vvvvvvvvv
+ssh n01635529@apollo.humber.ca
+:: anything after this does not display
---
+++ ssh munro.bat
@@ -0,0 +1,51 @@
+@echo off
+color b
+title connecting to munro
+echo @munro.humber.ca
+echo.
+echo                   connecting to munro
+echo                 connecting to munro
+echo               connecting to munro
+echo             connecting to munro
+echo           connecting to munro
+echo         connecting to munro
+echo       connecting to munro
+echo     connecting to munro
+echo   connecting to munro
+echo.  
+echo made by kokotf
+:: timeout not required just for aesthetic reasons
+timeout /t 3 /nobreak >nul
+cls
+echo.
+echo           _____                     _____                     _____          
+echo          /\    \                   /\    \                   /\    \         
+echo         /::\    \                 /::\    \                 /::\____\        
+echo        /::::\    \               /::::\    \               /:::/    /        
+echo       /::::::\    \             /::::::\    \             /:::/    /         
+echo      /:::/\:::\    \           /:::/\:::\    \           /:::/    /          
+echo     /:::/__\:::\    \         /:::/__\:::\    \         /:::/____/           
+echo     \:::\   \:::\    \        \:::\   \:::\    \       /::::\    \           
+echo   ___\:::\   \:::\    \     ___\:::\   \:::\    \     /::::::\    \   _____  
+echo  /\   \:::\   \:::\    \   /\   \:::\   \:::\    \   /:::/\:::\    \ /\    \ 
+echo /::\   \:::\   \:::\____\ /::\   \:::\   \:::\____\ /:::/  \:::\    /::\____\
+echo \:::\   \:::\   \::/    / \:::\   \:::\   \::/    / \::/    \:::\  /:::/    /
+echo  \:::\   \:::\   \/____/   \:::\   \:::\   \/____/   \/____/ \:::\/:::/    / 
+echo   \:::\   \:::\    \        \:::\   \:::\    \                \::::::/    /  
+echo    \:::\   \:::\____\        \:::\   \:::\____\                \::::/    /   
+echo     \:::\  /:::/    /         \:::\  /:::/    /                /:::/    /    
+echo      \:::\/:::/    /           \:::\/:::/    /                /:::/    /     
+echo       \::::::/    /             \::::::/    /                /:::/    /      
+echo        \::::/    /               \::::/    /                /:::/    /       
+echo         \::/    /                 \::/    /                 \::/    /        
+echo          \/____/                   \/____/                   \/____/        
+echo.
+title connected to munro
+::
+::
+::
+::
+:: Change student number to your own to connect to your profile
+::  vvvvvvvvv
+ssh n01635529@munro.humber.ca
+:: anything after this does not display
