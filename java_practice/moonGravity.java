d5468d77c1a5cf309fd609db0e961116c904997d
n01635529 <n01635529@humber.ca>
2023-10-16 13:44:17 -0400

    update to make better

--- java_practice/moonGravity.java
+++ java_practice/moonGravity.java
@@ -1,10 +1,16 @@
+import java.util.Scanner;
 
 public class moonGravity {
+
+    private final static double moonGravity = 0.17;
+
     public static void main(String[] args) {
 
-    double myWeight = 155;
-    double moonGravity = 0.17;
-    System.out.println("Your effective weight on the moon is: " + myWeight * moonGravity);
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Input your weight: ");
+        double myWeight = sc.nextDouble();
+        sc.close(); 
+        System.out.println("Your effective weight on the moon is: " + myWeight * moonGravity);
     
     }
 }
